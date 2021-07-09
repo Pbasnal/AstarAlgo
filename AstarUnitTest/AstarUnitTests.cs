97653b78c86a0fa04a3f8d96f53e735e4a394332
Pankaj Basnal <pankajbasnal17@gmail.com>
2021-07-09 16:25:18 +0530

    Adding the first iteration of the algorithm with simple test cases

--- AstarUnitTest/AstarUnitTests.cs
+++ AstarUnitTest/AstarUnitTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AstarAlgo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,86 +8,147 @@ namespace AstarUnitTest
     [TestClass]
     public class AstarUnitTests
     {
+        /// (A) -- 7 --> (B) 
+        /// (A) -- 7 --> (C) 
+        /// (B) -- 8 --> (G)
+        /// (C) -- 12 --> (G)
+        /// 
+        /// Shortest path from (A) to (G)
+        /// (A) -- 7 --> (B) -- 8 --> (G)
         [TestMethod]
         public void Simple_4_node_graph()
         {
-            var graph = GenerateTestGraph();
+            var allNodes = new List<LocationNode>();
+            var allEdges = new List<DirectedEdge>();
+
+            var nodeA = new LocationNode("A", 0, 0);
+            var nodeB = new LocationNode("B", 2, 5);
+            var nodeC = new LocationNode("C", 5, 1);
+            var nodeG = new LocationNode("G", 7, 7);
+            allNodes.Add(nodeA);
+            allNodes.Add(nodeB);
+            allNodes.Add(nodeC);
+            allNodes.Add(nodeG);
+
+            var edgeAB = new DirectedEdge(nodeA, nodeB, 7);
+            var edgeAC = new DirectedEdge(nodeA, nodeC, 7);
+            var edgeBG = new DirectedEdge(nodeB, nodeG, 8);
+            var edgeCG = new DirectedEdge(nodeC, nodeG, 12);
+            allEdges.Add(edgeAB);
+            allEdges.Add(edgeAC);
+            allEdges.Add(edgeBG);
+            allEdges.Add(edgeCG);
+
+            var graph = new LocationAstar(allNodes, allEdges);
             var path = graph.FindPathBetween(0, 3);
 
             Assert.AreEqual(2, path.Count);
 
-            foreach (var edge in path)
-            {
-                Assert.AreNotEqual("3", edge.FromNode.NodeId);
-                Assert.AreNotEqual("3", edge.ToNode.NodeId);
-            }
+            Assert.AreEqual("A", path[0].OriginNode.NodeId);
+            Assert.AreEqual("B", path[0].DestinationNode.NodeId);
+            Assert.AreEqual(7, path[0].Weight);
+
+            Assert.AreEqual("B", path[1].OriginNode.NodeId);
+            Assert.AreEqual("G", path[1].DestinationNode.NodeId);
+            Assert.AreEqual(8, path[1].Weight);
         }
 
-        /// <summary>
-        /// (1) -- 1 --> (2) 
-        /// (1) -- 1 --> (3) 
-        /// (2) -- 2 --> (4)
-        /// (3) -- 3 --> (4)
+        /// (A) -- 7 --> (B) 
+        /// (A) -- 7 --> (C) 
+        /// (B) -- 12 --> (G)
+        /// (C) -- 8 --> (G)
         /// 
-        /// Shortest path from (1) to (4)
-        /// (1) -- 1 --> (2) -- 2 --> (4)
-        /// </summary>
-        /// <returns></returns>
-        public Graph GenerateTestGraph()
+        /// Shortest path from (A) to (G)
+        /// (A) -- 7 --> (C) -- 8 --> (G)
+        [TestMethod]
+        public void Simple_4_node_graph2()
         {
-            var allNodes = new List<INode>();
-            var allEdges = new List<IEdge>();
+            var allNodes = new List<LocationNode>();
+            var allEdges = new List<DirectedEdge>();
 
-            var node1 = new Node("1");
-            var node2 = new Node("2");
-            var node3 = new Node("3");
-            var node4 = new Node("4");
-            allNodes.Add(node1);
-            allNodes.Add(node2);
-            allNodes.Add(node3);
-            allNodes.Add(node4);
+            var nodeA = new LocationNode("A", 0, 0);
+            var nodeB = new LocationNode("B", 2, 5);
+            var nodeC = new LocationNode("C", 5, 1);
+            var nodeG = new LocationNode("G", 7, 7);
+            allNodes.Add(nodeA);
+            allNodes.Add(nodeB);
+            allNodes.Add(nodeC);
+            allNodes.Add(nodeG);
 
-            var edge12 = new Edge(node1, node2, 1);
-            var edge13 = new Edge(node1, node3, 1);
-            var edge24 = new Edge(node2, node4, 2);
-            var edge34 = new Edge(node3, node4, 3);
-            allEdges.Add(edge12);
-            allEdges.Add(edge13);
-            allEdges.Add(edge24);
-            allEdges.Add(edge34);
+            var edgeAB = new DirectedEdge(nodeA, nodeB, 7);
+            var edgeAC = new DirectedEdge(nodeA, nodeC, 7);
+            var edgeBG = new DirectedEdge(nodeB, nodeG, 12);
+            var edgeCG = new DirectedEdge(nodeC, nodeG, 8);
+            allEdges.Add(edgeAB);
+            allEdges.Add(edgeAC);
+            allEdges.Add(edgeBG);
+            allEdges.Add(edgeCG);
 
-            return new Graph(allNodes, allEdges);
+            var graph = new LocationAstar(allNodes, allEdges);
+            var path = graph.FindPathBetween(0, 3);
+            Assert.AreEqual(2, path.Count);
+
+            Assert.AreEqual("A", path[0].OriginNode.NodeId);
+            Assert.AreEqual("C", path[0].DestinationNode.NodeId);
+            Assert.AreEqual(7, path[0].Weight);
+
+            Assert.AreEqual("C", path[1].OriginNode.NodeId);
+            Assert.AreEqual("G", path[1].DestinationNode.NodeId);
+            Assert.AreEqual(8, path[1].Weight);
         }
     }
 
-    public class Node : INode
+    public class LocationNode : INode
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
         public string NodeId { get; set; }
 
-        //public float HeuristicCost(INode goalNode)
-        //{
-        //    return 1; // should be changed to something better
-        //}
+        int INode.Id { get; set; }
 
-        public Node(string nodeId)
+        public LocationNode()
+        { }
+
+        public LocationNode(string nodeId, int x, int y)
         {
             NodeId = nodeId;
+            X = x;
+            Y = y;
         }
     }
 
-    public class Edge : IEdge
+    public class DirectedEdge : IWeightedEdge
     {
-        public INode FromNode { get; }
+        public double Weight { get; set; }
 
-        public INode ToNode { get; }
+        public INode OriginNode { get; set; }
+        public INode DestinationNode { get; set; }
 
-        public float Cost { get; }
+        public DirectedEdge()
+        { }
 
-        public Edge(Node fromNode, Node toNode, float cost)
+        public DirectedEdge(INode originNode, INode destinationNode, float cost)
         {
-            FromNode = fromNode;
-            ToNode = toNode;
-            Cost = cost;
+            OriginNode = originNode;
+            DestinationNode = destinationNode;
+            Weight = cost;
+        }
+    }
+
+    public class LocationAstar : AstarPath<LocationNode, DirectedEdge>
+    {
+        public LocationAstar(List<LocationNode> nodes, List<DirectedEdge> edges)
+            : base(nodes, edges)
+        {
+        }
+
+        public override double HeuristicCost(LocationNode fromNode, LocationNode toNode)
+        {
+            var xDiffSquared = Math.Pow(toNode.X - fromNode.X, 2);
+            var yDiffSquared = Math.Pow(toNode.Y - fromNode.Y, 2);
+
+            return Math.Sqrt(xDiffSquared + yDiffSquared);
         }
     }
 }
